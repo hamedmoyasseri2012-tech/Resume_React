7e6471bc43c58d0f572a1e41e9a56ac3fb618912
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-23 00:42:31 +0330

    v1.6

--- src/Pages/CreateTemplate.jsx
+++ src/Pages/CreateTemplate.jsx
@@ -10,7 +10,7 @@ const CreateTemplate = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="p-2">
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-4 p-2">
             <h3 className="font-bold border-b border-[#cecece] w-103.25 pt-5">
@@ -76,21 +76,27 @@ const CreateTemplate = () => {
             </button>
             <div className="font-bold border-b border-[#cecece] w-103.25 pt-5 flex justify-between pb-3">
               <p>Projects</p>
-              <span className="bg-[#319795] border rounded-xl"></span>
+              <button className="relative h-6 w-10.5 rounded-full bg-teal-500">
+                <span className="absolute right-0.75 top-0.75 h-4.5 w-4.5 rounded-full bg-white shadow" />
+              </button>
             </div>
             <button className="border-2 bg-[#309794] w-103.5 h-11 rounded-xl p-2 border-[#309794] text-white font-bold ]">
               Add Projects
             </button>
             <div className="font-bold border-b border-[#cecece] w-103.25 pt-5 flex justify-between pb-3">
               <p>Work Experience</p>
-              <span className="bg-[#319795] border rounded-xl"></span>
+              <button className="relative h-6 w-10.5 rounded-full bg-teal-500">
+                <span className="absolute right-0.75 top-0.75 h-4.5 w-4.5 rounded-full bg-white shadow" />
+              </button>
             </div>
             <button className="border-2 bg-[#309794] w-103.5 h-11 rounded-xl p-2 border-[#309794] text-white font-bold ]">
               Add Experience
             </button>
             <div className="font-bold border-b border-[#cecece] w-103.25 pt-5 flex justify-between pb-3">
               <p>Awards & Achievement</p>
-              <span className="bg-[#319795] border rounded-xl"></span>
+              <button className="relative h-6 w-10.5 rounded-full bg-teal-500">
+                <span className="absolute right-0.75 top-0.75 h-4.5 w-4.5 rounded-full bg-white shadow" />
+              </button>
             </div>
             <textarea
               className="border w-103.5 p-2 rounded-xl h-20 border-[#97a0b9] hover:border-[#0d6efd]"
@@ -99,30 +105,81 @@ const CreateTemplate = () => {
               placeholder="Use comma to separate Achievement"
             ></textarea>
           </div>
-          <div className="flex flex-col gap-2 shadow-xl border justify-center items-center w-200 ">
-            <h1 className="text-4xl">Your Name</h1>
-            <div className="flex gap-4 items-center ">
-              <div className="flex gap-3 items-center pb-3">
-                <i>
+          <div className="flex flex-col gap-2 shadow-xl/50 pt-4 border justify-center items-center w-200 p-2 ">
+            <h1 className="text-5xl font-bold">Your Name</h1>
+            <div className="flex gap-4 items-center justify-center pb-3">
+              <div className="flex gap-3 items-center  ">
+                <i className="text-[#6c757c]">
                   <ImLocation />
                 </i>
-                <p>Address line</p>
-                <div className="border-r h-7"></div>
+                <p className="text-[#d4d7d3] text-xl">Address line</p>
+                <div className="border-r h-7 border-[#d4d7d3]"></div>
               </div>
               <div className="flex gap-3 items-center ">
-                <i>
+                <i className="text-[#6c757c]">
                   <MdEmail />
                 </i>
-                <p>Email Address</p>
-                <div className="border-r h-7"></div>
+                <p className="text-[#d4d7d3] text-xl">Email Address</p>
+                <div className="border-r h-7 border-[#d4d7d3]"></div>
               </div>
               <div className="flex gap-3 items-center ">
-                <i>
+                <i className="text-[#6c757c]">
                   <FaPhone />
                 </i>
-                <p>Phone Number</p>
-                <div className="border-r h-7"></div>
+                <p className="text-[#d4d7d3] text-xl">Phone Number</p>
+                <div className="border-r h-7 border-[#d4d7d3]"></div>
               </div>
+            </div>
+            <div className="bg-[#d1e4e2] flex items-start justify-self-start text-start w-full p-2 ">
+              <h1 className="text-2xl font-bold">TECHNICAL SKILLS</h1>
+            </div>
+            <div>
+              <ul className="flex gap-4 justify-center items-center font-bold text-xl">
+                <li className="bg-[#d5e4da] p-2">Your</li>
+                <li className="bg-[#d5e4da] p-2">Skills</li>
+                <li className="bg-[#d5e4da] p-2">are</li>
+                <li className="bg-[#d5e4da] p-2">shown</li>
+                <li className="bg-[#d5e4da] p-2">here</li>
+              </ul>
+            </div>
+            <div className="bg-[#d1e4e2] flex items-start justify-self-start text-start w-full p-2 ">
+              <h1 className="text-2xl font-bold">PROJECTS</h1>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl">Project Title</h1>
+              <ul className="list-disc text-[#1A202C] font-medium">
+                <li>Project Description are Shown here</li>
+                <li>with Bullet Points</li>
+              </ul>
+            </div>
+            <div className="bg-[#d1e4e2] flex items-start justify-self-start text-start w-full p-2 ">
+              <h1 className="text-2xl font-bold">EDUCATION</h1>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl">Education Title</h1>
+              <ul className="list-disc text-[#1A202C] font-medium">
+                <li>Project Description are Shown here</li>
+                <li>with Bullet Points</li>
+              </ul>
+            </div>
+            <div className="bg-[#d1e4e2] flex items-start justify-self-start text-start w-full p-2 ">
+              <h1 className="text-2xl font-bold">WORK EXPERIENCE</h1>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl">Work Title</h1>
+              <ul className="list-disc text-[#1A202C] font-medium">
+                <li>Project Description are Shown here</li>
+                <li>with Bullet Points</li>
+              </ul>
+            </div>
+            <div className="bg-[#d1e4e2] flex items-start justify-self-start text-start w-full p-2 ">
+              <h1 className="text-2xl font-bold">AWARDS & ACHIEVEMENTS</h1>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl">Work Title</h1>
+              <ul className="list-disc text-[#1A202C] font-medium">
+                <li>Your Awards are shown here</li>
+              </ul>
             </div>
           </div>
         </div>
